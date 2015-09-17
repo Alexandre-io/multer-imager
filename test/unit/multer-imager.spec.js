6d9af893389d55d359c271e4bffa966f4a467bb6
Alexandre-io <alexandre@gotoserv.com>
2015-09-17 11:19:37 +0200

    chore(tests): tests multi instances

--- test/unit/multer-imager.spec.js
+++ test/unit/multer-imager.spec.js
@@ -22,6 +22,41 @@ describe('multer-imager', function() {
       }
     });
   });
+  it('stores the options within each instance', function() {
+    var multer = multerS3({
+      bucket: 'bucket',
+      dirname: 'uploads/',
+      accessKeyId: 'accessKeyId',
+      secretAccessKey: 'secretAccessKey',
+      region: 'region',
+      gm: {
+        format: 'png',
+        scale: {
+          width: 200,
+          height: 200,
+          type: 'contain'
+        }
+      }
+    });
+    var multer2 = multerS3({
+      bucket: 'bucket2',
+      dirname: 'uploads/',
+      accessKeyId: 'accessKeyId',
+      secretAccessKey: 'secretAccessKey',
+      region: 'region',
+      gm: {
+        format: 'png',
+        scale: {
+          width: 200,
+          height: 200,
+          type: 'contain'
+        }
+      }
+    });
+    multer.should.have.property('options');
+    multer.options.bucket.should.equal('bucket');
+    multer2.options.bucket.should.equal('bucket2');
+  });
   it('implements _handleFile', function() {
     var upload = multerS3({
       bucket: 'bucket',
