3fae3dce0b83abf3816dbfef946e9a712485e867
Alexandre-io <alexandre@gotoserv.com>
2015-09-17 11:17:36 +0200

    fix(lib): remove global variables

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var crypto = require('crypto');
 var gm = require('graphicsmagick-stream');
 var _ = require("lodash");
 
-var options = {};
-var s3fs;
-var convert;
-
 function S3Storage(opts) {
   if (!opts.bucket) throw new Error('bucket is required');
   if (!opts.secretAccessKey) throw new Error('secretAccessKey is required');
@@ -14,35 +10,37 @@ function S3Storage(opts) {
   if (!opts.region) throw new Error('region is required');
   if (!opts.dirname) throw new Error('dirname is required');
   if (!opts.gm) throw new Error('gm is required');
-  options = opts;
-  s3fs = new S3FS(options.bucket, options);
-  convert = gm(options.gm);
+  this.options = opts;
+  this.s3fs = new S3FS(opts.bucket, opts);
+  this.convert = gm(opts.gm);
 }
 
 S3Storage.prototype._handleFile = function(req, file, cb) {
+  var self = this;
   var fileName;
-  if (options.reqfilename) {
-    fileName = _.get(req, options.reqfilename);
+  if (self.options.reqfilename) {
+    fileName = _.get(req, self.options.reqfilename);
   }
   else {
     fileName = crypto.randomBytes(20).toString('hex');
   }
-  var outStream = s3fs.createWriteStream(options.dirname + '/' + fileName);
+  var filePath = self.options.dirname + '/' + fileName;
+  var outStream = self.s3fs.createWriteStream(filePath);
   file.stream
-    .pipe(convert())
+    .pipe(self.convert())
     .pipe(outStream);
   outStream.on('error', cb);
   outStream.on('finish', function() {
     cb(null, {
       size: outStream.bytesWritten,
-      key: options.dirname + '/' + fileName,
-      location: 'https://' + options.bucket + '.s3.amazonaws.com/' + options.dirname + '/' + fileName
+      key: filePath,
+      location: 'https://' + self.options.bucket + '.s3.amazonaws.com/' + filePath
     });
   });
 };
 
 S3Storage.prototype._removeFile = function(req, file, cb) {
-  s3fs.unlink(file.path, cb);
+  this.s3fs.unlink(file.key, cb);
 };
 
 module.exports = function(opts) {
